7e0771588b18b703eab99fe2ceb8cf2b55a2d49e
Quinn <henth013@d.umn.edu>
2023-04-11 19:06:48 -0500

    Changed some pinout stuff

--- include/PINOUT.h
+++ include/PINOUT.h
@@ -1,7 +1,6 @@
 /**
  * @author: Quinn Henthorne henth013@d.umn.edu 7636568391
 */
-#define SERVO_PIN 19 //Controls the angle of the sonar servo
 #define LEFT_ENC_A_PIN 18 //A pin for the left motor encoder
 #define LEFT_ENC_B_PIN 5 //B pin for the left motor encoder
 #define RIGHT_ENC_A_PIN 17 //A pin for the right motor encoder
@@ -11,3 +10,5 @@
 
 #define STEER_MOTOR_PWM_PIN 25 //Controls the speed of the left motor
 #define DRIVE_MOTOR_PWM_PIN 13 //Controls the speed of the right motor
+#define SCL_PIN 22 //I2C clock pin
+#define SDA_PIN 21 //I2C data pin
